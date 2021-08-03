e419cb1ecbad95b1ea8c7d5c0aeb355a0742781a
austinxduong <axduong2@gmail.com>
2021-08-03 11:34:18 -0700

    fixed page breakage

--- src/components/MemeApp.js
+++ src/components/MemeApp.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Header from "./Header";
-import MemeGenerator from "./MemeGenerator"
+// import Header from "./Header";
+// import MemeGenerator from "./MemeGenerator"
 
 const MemeApp = () => {
     return (
         <div>
-            <Header />
-            <MemeGenerator />
+            {/* <Header />
+            <MemeGenerator /> */}
         </div>
-    )
-}
+    );
+};
 
 export default MemeApp;
