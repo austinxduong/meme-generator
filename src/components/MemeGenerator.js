ea001ca5e096156db5f0cd0f85ccd3728a885e8d
austinxduong <axduong2@gmail.com>
2021-08-03 11:59:14 -0700

    add randomize function, to randomize memes

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -3,6 +3,28 @@
 // import React, { useState, useEffect } from "react";
 
 const MemeGenerator = () => {
+    const [inputText, setinputText] = useState({
+        topText: "",
+        bottomText: ""
+    });
+    const [randomImage, setRandomImage] = useState(
+        "https://i.imgflip.com/26am.jpg"
+    );
+    const [allMemeImgs, setAllMemeImgs] = useState([]);
+
+    const handleChange = e => {
+        setinputText({
+            ...inputText,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const randNum = Math.floor(Math.random() * allMemeImgs.length);
+        const randMemeImgUrl = allMemeImgs[randNum].url;
+        setRandomImage(randMemeImgUrl);
+    }
 
 }
 
